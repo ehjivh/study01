ad689832ddb2b28660f840df8872f1ae71c9d847
ehjivh <40134385+ehjivh@users.noreply.github.com>
2018-08-23 21:47:42 +0900

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,3 @@
+#print("Hello World")
+msg="Hello"
+print(msg)
---
+++ test-2.py
@@ -0,0 +1,13 @@
+s="He\nllo Wor\tld"
+html="""<html>
+<body><body>
+</html>"""
+print(s)
+print(html)
+
+#整数
+i=10
+print(i/3)
+print(i**2)
+
+y=2
